41ff8bfb411c96d48d8831343d97dc687cb328e4
siddrashid <siddrashid0601@gmail.com>
2021-03-11 11:08:14 +0530

    Pizza added

--- hungry.py
+++ hungry.py
@@ -2,5 +2,6 @@ hungry = input("Are you hungry? (Y/N): ")
 if hungry == "Y":
     print("Eat samosas.")
     print("Eat burger.")
+    print("Eat pizza.")
 else:
     print("Do your homework please!")
