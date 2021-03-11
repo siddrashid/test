fa445257045315dad9e45fea21c1a04d90d33e76
siddrashid <siddrashid0601@gmail.com>
2021-03-11 13:20:10 +0530

    Updated hungry.py

--- hungry.py
+++ hungry.py
@@ -2,7 +2,6 @@ hungry = input("Are you hungry? (Y/N): ")
 if hungry == "Y":
     print("Eat samosas.")
     print("Eat burger.")
-    print("Eat pizza.")
 else:
     thirsty = input("Are you thirsty? (Y/N): ")
     if thirsty == "Y":
