cad4a82f3a15ddb5d1e123d5c49f9ab64a2f2136
siddrashid <siddrashid0601@gmail.com>
2021-03-11 11:16:08 +0530

    Revert "Pizza added"
    This reverts commit 41ff8bfb411c96d48d8831343d97dc687cb328e4.

--- hungry.py
+++ hungry.py
@@ -2,6 +2,5 @@ hungry = input("Are you hungry? (Y/N): ")
 if hungry == "Y":
     print("Eat samosas.")
     print("Eat burger.")
-    print("Eat pizza.")
 else:
     print("Do your homework please!")
